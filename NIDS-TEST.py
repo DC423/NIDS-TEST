3194d6848b3684f365d7db6b821b6cc404073e87
DC423 <tothehilt.423@gmail.com>
2016-06-15 11:50:38 -0400

    Create NIDS-TEST.py

---
+++ NIDS-TEST.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Author: @tothehilt
+# DC423 Project to test an NIDS if its working
+# Based off a snort rule located:
+# https://github.com/DC423/NIDS-TEST/blob/master/nids-test.rules
+#
+#########################################
+import argparse
+import socket
+import sys
+import random
+
+# Argument Checking
+if (len(sys.argv) != 2): 
+	print("USAGE: Python ./NIDS-TEST.py <host>")
+	sys.exit()
+
+# host is passed in via cli arguments
+host=sys.argv[1]
+# LTS port
+port=1337
+# Messages defined by IDS rules
+MESSAGE1 = "NIDS-TEST-PACKET-LOW"
+MESSAGE2 = "NIDS-TEST-PACKET-MED"
+MESSAGE3 = "NIDS-TEST-PACKET-HIGH"
+# create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# send all three messages
+sock.sendto(bytes(MESSAGE1, "utf-8"), (host, port))
+sock.sendto(bytes(MESSAGE2, "utf-8"), (host, port))
+sock.sendto(bytes(MESSAGE3, "utf-8"), (host, port))
+# close the socket, just incase you didn't get reset by peer..
+sock.close()
